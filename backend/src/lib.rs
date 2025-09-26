68aa258798f9cbb007c20658ec31a41b1556a7a2
bobby-palmer <112954547+bobby-palmer@users.noreply.github.com>
2025-09-26 19:52:14 -0400

    THIS IS AN AUTO-GENERATED COMMIT BECAUSE I WAS TOO LAZY TO MAKE ONE

--- backend/src/lib.rs
+++ backend/src/lib.rs
@@ -1,115 +1,13 @@
-pub mod api {
-
-    pub async fn login() {
-
-    }
-
-}
+pub mod api { }
 
 pub mod game {
-    use std::collections::HashMap;
+    pub struct Game { }
 
-    use anyhow::Result;
+    impl Game { }
 
-    pub struct Game {
-        players: HashMap<String, Player>,
-        mode: Mode,
-        flips: Vec<Vec<Flip>>,
-        current_flip: usize,
-    }
-
-    impl Game {
-        pub fn add_player(&mut self, name: String) -> Result<()> {
-            if self.players.contains_key(&name) {
-                anyhow::bail!("This username is already taken");
-            }
-
-            if self.mode != Mode::Joining {
-                anyhow::bail!("This game is already in progress");
-            }
-
-            self.players.insert(name, Player::default());
-            Ok(())
-        }
-
-        pub fn bet(&mut self, name: String, bet: Bet) -> Result<()> {
-            if self.mode != Mode::Betting {
-                anyhow::bail!("Betting is not open right now");
-            }
-
-            for idx in bet.flips.iter() {
-                if *idx >= self.flips[self.current_flip].len() {
-                    anyhow::bail!("Invalid flip index");
-                }
-            }
-
-            let player = self.players.get_mut(&name).ok_or(anyhow::anyhow!("You must be signed in to play this game"))?;
-
-            if player.stack < bet.amount {
-                anyhow::bail!("You cannot bet more than your stack");
-            }
-
-            player.current_bet = Some(bet);
-            Ok(())
-        }
-
-        pub fn tick(&mut self) {
-            match self.mode {
-
-                Mode::Joining => {
-                    if self.flips.len() == 0 {
-                        self.mode = Mode::Done;
-                    } else {
-                        self.mode = Mode::Betting;
-                        self.current_flip = 0;
-                    }
-                } // Joining
-
-                Mode::Betting => {
-                    self.mode = Mode::BetResult;
-                } // Betting
-
-                Mode::BetResult => {
-                    if self.current_flip + 1 >= self.flips.len() {
-                        self.mode = Mode::Done
-                    } else {
-                        self.mode = Mode::Betting;
-                        self.current_flip += 1;
-                    }
-                } // BetResult
-
-                // No op for safety
-                Mode::Done => {}
-
-            } // match self.node
-        }
-    }
-
-    struct Player {
-        stack: u64,
-        current_bet: Option<Bet>,
-    }
-
-    impl Default for Player {
-        fn default() -> Self {
-            Self {
-                stack: 10000,
-                current_bet: None,
-            }
-        }
-    }
-
-    pub struct Bet {
-        amount: u64,
-        flips: Vec<usize>,
-    }
-
-    #[derive(PartialEq, Eq)]
     enum Mode {
         Joining,
-        Betting,
-        BetResult,
-        Done,
+        Betting(Vec<Flip>),
     }
 
     /// self.0 to self.1 odds
@@ -117,14 +15,13 @@ pub mod game {
 
     struct Flip {
         payout: Payout,
-
         /// percentage of success (out of 100)
         probability: u64,
     }
 
-    impl Flip {
-        fn evaluate(&self) -> Payout {
-            todo!()
-        }
+    struct EvaluatedFlip {
+        payout: Payout,
+        is_heads: bool,
     }
+
 }
