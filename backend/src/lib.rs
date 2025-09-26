5611cb0f9ca19296be0540ce25dd063169da1487
bobby-palmer <112954547+bobby-palmer@users.noreply.github.com>
2025-09-26 10:12:29 -0400

    THIS IS AN AUTO-GENERATED COMMIT BECAUSE I WAS TOO LAZY TO MAKE ONE

--- backend/src/lib.rs
+++ backend/src/lib.rs
@@ -14,6 +14,8 @@ pub mod game {
     pub struct Game {
         players: HashMap<String, Player>,
         mode: Mode,
+        flips: Vec<Vec<Flip>>,
+        current_flip: usize,
     }
 
     impl Game {
@@ -29,16 +31,62 @@ pub mod game {
             self.players.insert(name, Player::default());
             Ok(())
         }
+
+        pub fn bet(&mut self, name: String, bet: Bet) -> Result<()> {
+            if self.mode != Mode::Betting {
+                anyhow::bail!("Betting is not open right now");
+            }
+
+            for idx in bet.flips.iter() {
+                if *idx >= self.flips[self.current_flip].len() {
+                    anyhow::bail!("Invalid flip index");
+                }
+            }
+
+            let player = self.players.get_mut(&name).ok_or(anyhow::anyhow!("You must be signed in to play this game"))?;
+
+            if player.stack < bet.amount {
+                anyhow::bail!("You cannot bet more than your stack");
+            }
+
+            player.current_bet = Some(bet);
+            Ok(())
+        }
+
+        pub fn tick(&mut self) {
+            match self.mode {
+                Mode::Joining => {
+                    self.mode = Mode::Betting;
+                    self.current_flip = 0;
+                }
+                Mode::Betting => {
+
+                }
+                Mode::BetResult => {
+
+                }
+                Mode::Done => {}
+            }
+        }
     }
 
     struct Player {
-        stack: u64
+        stack: u64,
+        current_bet: Option<Bet>,
     }
 
     impl Default for Player {
         fn default() -> Self {
-            todo!()
+            Self {
+                stack: 10000,
+                current_bet: None,
+            }
         }
+    }
+
+    pub struct Bet {
+        amount: u64,
+        flips: Vec<usize>,
     }
 
     #[derive(PartialEq, Eq)]
@@ -47,5 +95,12 @@ pub mod game {
         Betting,
         BetResult,
         Done,
+    }
+
+    struct Payout(u64, u64);
+
+    struct Flip {
+        payout: Payout,
+        probability: u64,
     }
 }
